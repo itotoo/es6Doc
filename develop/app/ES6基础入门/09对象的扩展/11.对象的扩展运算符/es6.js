cd730734ad92eb3525e377744397963ea42b43d0
liushengjie <liushengjie@mistong.com>
2018-04-10 14:40:44 +0800

    'es6Doc'

--- develop/app/ES6基础入门/09对象的扩展/11.对象的扩展运算符/es6.js
+++ develop/app/ES6基础入门/09对象的扩展/11.对象的扩展运算符/es6.js
@@ -4,5 +4,5 @@ import shim from "es5-shim";
 
 // ES2018 将这个运算符引入了对象。
 // 目前不支持对象
-let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
-console.log(x,y,z)
+// let { x, y, ...z } = { x: 1, y: 2, a: 3, b: 4 };
+// console.log(x,y,z)
